86148ac8360dd0e77beae66b25e84db3e7e65000
MostafaHakim <mdmostafahakim@gmail.com>
2026-01-16 14:50:45 +0600

    adjustedment

--- frontend/src/pages/Notification.jsx
+++ frontend/src/pages/Notification.jsx
@@ -16,73 +16,94 @@ const Notification = () => {
       {/* =================Notification Card======================= */}
       <div className="grid grid-cols-1 lg:px-6">
         <div className="p-2 lg:p-4 border border-gray-100 rounded-2xl relative overflow-hidden">
-          <div className="lg:p-4 z-20">
+          <div className="lg:p-4 z-30 overflow-hidden relative">
             <div className="flex flex-row items-center justify-between py-2">
-              <h2 className="text-md lg:text-2xl text-black">প্রিয় অভিবাবক</h2>
-              <p className="text-sm text-textc2-700">
+              <h2 className="text-[16px] lg:text-2xl text-black">
+                প্রিয় অভিবাবক
+              </h2>
+              <p className="text-[14px] lg:text-[17px] text-textc2-700">
                 {new Date().toTimeString().split(" ")[0]}
               </p>
             </div>
-            <p className="text-sm text-justify mr-6">
+            <p className="text-[13px] lg:text-[17px] text-textc1-700 text-justify mr-6">
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis
               repellendus illum
             </p>
           </div>
-          <img className="absolute top-0 left-0 z-10" src={Weather} alt="" />
+          <img
+            className="absolute top-0 left-0 w-[90px] lg:w-[180px] z-10"
+            src={Weather}
+            alt=""
+          />
         </div>
       </div>
-
       <div className="grid grid-cols-1 lg:px-6">
         <div className="p-2 lg:p-4 border border-gray-100 rounded-2xl relative overflow-hidden">
-          <div className="lg:p-4 z-20">
+          <div className="lg:p-4 z-30 overflow-hidden relative">
             <div className="flex flex-row items-center justify-between py-2">
-              <h2 className="text-md lg:text-2xl text-black">প্রিয় অভিবাবক</h2>
-              <p className="text-sm text-textc2-700">
+              <h2 className="text-[16px] lg:text-2xl text-black">
+                প্রিয় অভিবাবক
+              </h2>
+              <p className="text-[14px] lg:text-[17px] text-textc2-700">
                 {new Date().toTimeString().split(" ")[0]}
               </p>
             </div>
-            <p className="text-sm text-justify mr-6">
+            <p className="text-[13px] lg:text-[17px] text-textc1-700 text-justify mr-6">
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis
               repellendus illum
             </p>
           </div>
-          <img className="absolute top-0 left-0 z-10" src={Weather} alt="" />
+          <img
+            className="absolute top-0 left-0 w-[90px] lg:w-[180px] z-10"
+            src={Weather}
+            alt=""
+          />
         </div>
       </div>
-
       <div className="grid grid-cols-1 lg:px-6">
         <div className="p-2 lg:p-4 border border-gray-100 rounded-2xl relative overflow-hidden">
-          <div className="lg:p-4 z-20">
+          <div className="lg:p-4 z-30 overflow-hidden relative">
             <div className="flex flex-row items-center justify-between py-2">
-              <h2 className="text-md lg:text-2xl text-black">প্রিয় অভিবাবক</h2>
-              <p className="text-sm text-textc2-700">
+              <h2 className="text-[16px] lg:text-2xl text-black">
+                প্রিয় অভিবাবক
+              </h2>
+              <p className="text-[14px] lg:text-[17px] text-textc2-700">
                 {new Date().toTimeString().split(" ")[0]}
               </p>
             </div>
-            <p className="text-sm text-justify mr-6">
+            <p className="text-[13px] lg:text-[17px] text-textc1-700 text-justify mr-6">
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis
               repellendus illum
             </p>
           </div>
-          <img className="absolute top-0 left-0 z-10" src={Weather} alt="" />
+          <img
+            className="absolute top-0 left-0 w-[90px] lg:w-[180px] z-10"
+            src={Weather}
+            alt=""
+          />
         </div>
       </div>
-
       <div className="grid grid-cols-1 lg:px-6">
         <div className="p-2 lg:p-4 border border-gray-100 rounded-2xl relative overflow-hidden">
-          <div className="lg:p-4 z-20">
+          <div className="lg:p-4 z-30 overflow-hidden relative">
             <div className="flex flex-row items-center justify-between py-2">
-              <h2 className="text-md lg:text-2xl text-black">প্রিয় অভিবাবক</h2>
-              <p className="text-sm text-textc2-700">
+              <h2 className="text-[16px] lg:text-2xl text-black">
+                প্রিয় অভিবাবক
+              </h2>
+              <p className="text-[14px] lg:text-[17px] text-textc2-700">
                 {new Date().toTimeString().split(" ")[0]}
               </p>
             </div>
-            <p className="text-sm text-justify mr-6">
+            <p className="text-[13px] lg:text-[17px] text-textc1-700 text-justify mr-6">
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis
               repellendus illum
             </p>
           </div>
-          <img className="absolute top-0 left-0 z-10" src={Weather} alt="" />
+          <img
+            className="absolute top-0 left-0 w-[90px] lg:w-[180px] z-10"
+            src={Weather}
+            alt=""
+          />
         </div>
       </div>
     </div>
