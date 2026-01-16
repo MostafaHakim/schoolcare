fe3be5dcce893c3c713100aa614c1cf718d61fd7
MostafaHakim <mdmostafahakim@gmail.com>
2026-01-16 16:17:31 +0600

    restore

---
+++ frontend/src/contexts/classContext.jsx
@@ -0,0 +1,97 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import { useAuth } from "./AuthContext";
+
+const ClassContext = createContext();
+
+export const ClassProvider = ({ children }) => {
+  const [classes, setClasses] = useState([]); // ✅ array
+  const [loading, setLoading] = useState(false);
+  const { user } = useAuth();
+
+  // ✅ fetch classes
+  useEffect(() => {
+    if (!user?.school) {
+      return;
+    }
+
+    const fetchClasses = async () => {
+      try {
+        const res = await fetch(
+          `${
+            import.meta.env.VITE_BASE_URL
+          }/api/classes?school=${encodeURIComponent(user?.school)}`
+        );
+
+        if (!res.ok) throw new Error("Failed to fetch");
+
+        const data = await res.json();
+        setClasses(data);
+      } catch (err) {
+        setClasses([]);
+        toast.error("Failed to load classes");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchClasses();
+  }, [user?.school]);
+
+  //  add new class
+  const addNewClass = async (formData) => {
+    console.log(formData);
+    try {
+      const res = await fetch(`${import.meta.env.VITE_BASE_URL}/api/classes`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        toast.error(data.message || "Failed to create class");
+        return null;
+      }
+
+      setClasses((prev) => [...prev, data]);
+
+      toast.success("Class added successfully 🎉");
+      return data;
+    } catch (error) {
+      toast.error("Server error. Try again later");
+      return null;
+    }
+  };
+
+  const deleteClass = async (id) => {
+    try {
+      const res = await fetch(
+        `${import.meta.env.VITE_BASE_URL}/api/classes/${id}`,
+        {
+          method: "DELETE",
+        }
+      );
+
+      if (!res.ok) throw new Error("Failed to Delete");
+      setClasses((prev) => prev.filter((cls) => cls._id !== id));
+      const data = await res.json();
+
+      toast.success("Class Delete Successfully");
+    } catch (err) {
+      setClasses([]);
+      toast.error("Failed to load classes");
+    }
+  };
+
+  return (
+    <ClassContext.Provider
+      value={{ classes, loading, addNewClass, deleteClass }}
+    >
+      {children}
+    </ClassContext.Provider>
+  );
+};
+
+export const useClass = () => useContext(ClassContext);
