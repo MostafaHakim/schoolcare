d1f7a36ee16ef04fca73c5bb61d4ed283c668585
MostafaHakim <mdmostafahakim@gmail.com>
2026-01-21 18:15:40 +0600

    my commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -290,10 +290,10 @@ function App() {
                               />
                             </Route>
 
-                            {/* <Route
+                            <Route
                               path="*"
                               element={<Navigate to="/login" replace />}
-                            /> */}
+                            />
                           </Routes>
                         </NotificationProvider>
                       </SocketProvider>
